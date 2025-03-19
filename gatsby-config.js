91929a807947a2787717a647113db9b4fdce7955
Gyula Soos <soosgyul@gmail.com>
2025-03-19 09:17:51 +0000

    remove unused configuration files and update package.json for Gatsby 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,45 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
-    siteMetadata: {
-        siteUrl: "https://gcs.soos.uk",
-        title: "Emergency Board Game Tools",
-        menuLinks: [
-            {
-                name: "Home",
-                link: "/",
-            },
-            {
-                name: "GSC Calculator",
-                link: "/",
-            },
-            {
-                name: "GSC Dice",
-                link: "/dice",
-            },
-            {
-                name: "Welcome To cards",
-                link: "/welcome",
-            },
-        ],
-    },
-    plugins: [
-        "gatsby-plugin-sass",
-        "gatsby-plugin-gatsby-cloud",
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-                fonts: [`Boogaloo`],
-                display: "swap",
-            },
-        },
+  siteMetadata: {
+    siteUrl: "https://gcs.soos.uk",
+    title: "Emergency Board Game Tools",
+    menuLinks: [
+      {
+        name: "Home",
+        link: "/",
+      },
+      {
+        name: "GSC Calculator",
+        link: "/",
+      },
+      {
+        name: "GSC Dice",
+        link: "/dice",
+      },
+      {
+        name: "Welcome To cards",
+        link: "/welcome",
+      },
     ],
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Boogaloo`],
+        display: "swap",
+      },
+    },
+  ],
 };
